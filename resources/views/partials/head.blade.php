009d807ad9f50cb5c265cc548eb2d7c2204a1a55
RizqiA17 <asyriansyah@gmail.com>
2025-08-29 17:24:29 +0700

    auto menggunakan hasil build

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,5 +10,20 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+@if(file_exists(public_path('build/manifest.json')))
+    @php
+        $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+        $cssFile = $manifest['resources/css/app.css']['file'] ?? null;
+        $jsFile = $manifest['resources/js/app.js']['file'] ?? null;
+    @endphp
+    @if($cssFile)
+        <link rel="stylesheet" href="{{ asset('build/' . $cssFile) }}">
+    @endif
+    @if($jsFile)
+        <script type="module" src="{{ asset('build/' . $jsFile) }}"></script>
+    @endif
+@else
+    {{-- Fallback to Vite for development --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+@endif
 @fluxAppearance
