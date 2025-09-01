cb89528d277357780729f44449fa3e818e51d0d5
RizqiA17 <asyriansyah@gmail.com>
2025-09-01 13:43:36 +0700

    hide share button

--- resources/views/livewire/settings/profile.blade.php
+++ resources/views/livewire/settings/profile.blade.php
@@ -78,7 +78,7 @@
                                     </svg>
                                     Edit Profile
                                 </a>
-                                <button type="button"
+                                {{-- <button type="button"
                                     class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                                     <svg class="-ml-1 mr-2 h-5 w-5" fill="none" stroke="currentColor"
                                         viewBox="0 0 24 24">
@@ -86,7 +86,7 @@
                                             d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
                                     </svg>
                                     Share
-                                </button>
+                                </button> --}}
                             </div>
                         </div>
 
