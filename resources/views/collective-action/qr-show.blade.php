139a1cd5739c6a9f56a396214ea158151b0d0b4c
RizqiA17 <asyriansyah@gmail.com>
2025-09-22 16:21:42 +0700

    perbaiki ukuran qr di desktop

--- resources/views/collective-action/qr-show.blade.php
+++ resources/views/collective-action/qr-show.blade.php
@@ -17,7 +17,7 @@ use SimpleSoftwareIO\QrCode\Facades\QrCode;
                     <h2 class="text-lg sm:text-xl font-semibold text-gray-800 dark:text-slate-200 mb-3 sm:mb-4">Scan QR Code untuk Bergabung</h2>
                     <div class="flex justify-center mb-3 sm:mb-4">
                         <div class="bg-white p-2 sm:p-4 rounded-lg border-2 border-gray-200 max-w-[200px] sm:max-w-none">
-                            <div class="w-32 h-32 sm:w-48 sm:h-48 mx-auto overflow-hidden flex items-center justify-center">
+                            <div class="w-48 h-48 sm:w-48 sm:h-48 mx-auto overflow-hidden flex items-center justify-center">
                                 {!! QrCode::size(300)->format('svg')->generate($qrUrl) !!}
                             </div>
                         </div>
--- resources/views/livewire/connections/qr-scanner.blade.php
+++ resources/views/livewire/connections/qr-scanner.blade.php
@@ -37,7 +37,7 @@
                         @if ($myQrSvg)
                             <div class="flex justify-center mb-3 sm:mb-4">
                                 <div class="bg-white p-2 sm:p-4 rounded-lg border-2 border-gray-200 max-w-[200px] sm:max-w-none">
-                                    <div class="w-32 h-32 sm:w-48 sm:h-48 mx-auto overflow-hidden flex items-center justify-center">
+                                    <div class="w-48 h-48 sm:w-48 sm:h-48 mx-auto overflow-hidden flex items-center justify-center">
                                         {!! $myQrSvg !!}
                                     </div>
                                 </div>
