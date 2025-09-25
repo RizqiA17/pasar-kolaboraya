ad8fae88e943266d5134caae8633304935c06598
RizqiA17 <asyriansyah@gmail.com>
2025-09-25 11:13:15 +0700

    Update layout.blade.php

--- resources/views/components/admin/layout.blade.php
+++ resources/views/components/admin/layout.blade.php
@@ -184,7 +184,7 @@
                                     Keahlian
                                 </a>
 
-                                <a href="{{ route('admin.contributions') }}"
+                                {{-- <a href="{{ route('admin.contributions') }}"
                                     class="group flex items-center px-3 py-2 text-sm font-medium rounded-lg transition-colors {{ request()->routeIs('admin.contributions*') ? 'bg-green-100 dark:bg-green-900/20 text-green-700 dark:text-green-300' : 'text-slate-600 dark:text-slate-400 hover:bg-slate-100 dark:hover:bg-slate-700/50 hover:text-slate-900 dark:hover:text-slate-200' }}">
                                     <svg class="mr-3 h-5 w-5" fill="none" stroke="currentColor"
                                         viewBox="0 0 24 24">
@@ -193,7 +193,7 @@
                                         </path>
                                     </svg>
                                     Kontribusi
-                                </a>
+                                </a> --}}
 
                                 <a href="{{ route('admin.peran') }}"
                                     class="group flex items-center px-3 py-2 text-sm font-medium rounded-lg transition-colors {{ request()->routeIs('admin.peran*') ? 'bg-indigo-100 dark:bg-indigo-900/20 text-indigo-700 dark:text-indigo-300' : 'text-slate-600 dark:text-slate-400 hover:bg-slate-100 dark:hover:bg-slate-700/50 hover:text-slate-900 dark:hover:text-slate-200' }}">
