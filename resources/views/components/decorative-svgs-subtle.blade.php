59d536af8b2fd2a9c4fda13dc3d2bee10773adf0
RizqiA17 <asyriansyah@gmail.com>
2025-09-29 15:27:39 +0700

    decorative preset

--- resources/views/components/decorative-svgs-subtle.blade.php
+++ resources/views/components/decorative-svgs-subtle.blade.php
@@ -2,30 +2,30 @@
 <div class="fixed inset-0 pointer-events-none overflow-hidden z-0">
     {{-- Top-Left Corner - Very Subtle --}}
     <div class="absolute top-0 left-0 w-32 h-32 opacity-20">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/1.svg') }}" alt="" class="w-full h-full object-contain">
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/1.webp') }}" alt="" class="w-full h-full object-contain">
     </div>
     
     {{-- Top-Right Corner - Very Subtle --}}
     <div class="absolute top-0 right-0 w-32 h-32 opacity-20">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/7.svg') }}" alt="" class="w-full h-full object-contain">
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/7.webp') }}" alt="" class="w-full h-full object-contain">
     </div>
     
     {{-- Bottom-Left Corner - Very Subtle --}}
     <div class="absolute bottom-0 left-0 w-32 h-32 opacity-20">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/5.svg') }}" alt="" class="w-full h-full object-contain">
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/5.webp') }}" alt="" class="w-full h-full object-contain">
     </div>
     
     {{-- Bottom-Right Corner - Very Subtle --}}
     <div class="absolute bottom-0 right-0 w-32 h-32 opacity-20">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/11.svg') }}" alt="" class="w-full h-full object-contain">
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/11.webp') }}" alt="" class="w-full h-full object-contain">
     </div>
     
     {{-- Center Edges - Very Subtle --}}
     <div class="absolute top-1/2 left-0 w-24 h-24 opacity-15">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/4.svg') }}" alt="" class="w-full h-full object-contain">
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/4.webp') }}" alt="" class="w-full h-full object-contain">
     </div>
     
     <div class="absolute top-1/2 right-0 w-24 h-24 opacity-15">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/10.svg') }}" alt="" class="w-full h-full object-contain">
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/10.webp') }}" alt="" class="w-full h-full object-contain">
     </div>
 </div>
--- resources/views/components/decorative-svgs.blade.php
+++ resources/views/components/decorative-svgs.blade.php
@@ -2,81 +2,81 @@
 <div class="fixed inset-0 pointer-events-none overflow-hidden z-0">
     {{-- Top-Left Corner --}}
     <div class="absolute top-0 left-0 w-64 h-64 opacity-60">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/1.svg') }}" alt="" class="w-full h-full object-contain">
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/1.webp') }}" alt="" class="w-full h-full object-contain">
     </div>
 
     {{-- Top-Left Mid --}}
     <div class="absolute top-32 left-32 w-48 h-48 opacity-50">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/2.svg') }}" alt="" class="w-full h-full object-contain">
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/2.webp') }}" alt="" class="w-full h-full object-contain">
     </div>
 
     {{-- Top-Left Bottom --}}
     <div class="absolute top-64 left-16 w-40 h-40 opacity-40">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/3.svg') }}" alt="" class="w-full h-full object-contain">
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/3.webp') }}" alt="" class="w-full h-full object-contain">
     </div>
 
     {{-- Mid-Left Edge --}}
     <div class="absolute top-1/3 left-8 w-32 h-32 opacity-50">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/4.svg') }}" alt="" class="w-full h-full object-contain">
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/4.webp') }}" alt="" class="w-full h-full object-contain">
     </div>
 
     {{-- Bottom-Left Corner --}}
     <div class="absolute bottom-0 left-0 w-56 h-56 opacity-60">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/5.svg') }}" alt="" class="w-full h-full object-contain">
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/5.webp') }}" alt="" class="w-full h-full object-contain">
     </div>
 
     {{-- Bottom-Left Mid --}}
     <div class="absolute bottom-32 left-24 w-44 h-44 opacity-50">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/6.svg') }}" alt="" class="w-full h-full object-contain">
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/6.webp') }}" alt="" class="w-full h-full object-contain">
     </div>
 
     {{-- Top-Right Corner --}}
     <div class="absolute top-0 right-0 w-64 h-64 opacity-60">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/7.svg') }}" alt="" class="w-full h-full object-contain">
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/7.webp') }}" alt="" class="w-full h-full object-contain">
     </div>
 
     {{-- Top-Right Mid --}}
     <div class="absolute top-32 right-32 w-48 h-48 opacity-50">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/8.svg') }}" alt="" class="w-full h-full object-contain">
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/8.webp') }}" alt="" class="w-full h-full object-contain">
     </div>
 
     {{-- Top-Right Bottom --}}
     <div class="absolute top-64 right-16 w-40 h-40 opacity-40">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/9.svg') }}" alt="" class="w-full h-full object-contain">
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/9.webp') }}" alt="" class="w-full h-full object-contain">
     </div>
 
     {{-- Mid-Right Edge --}}
     <div class="absolute top-1/3 right-8 w-32 h-32 opacity-50">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/10.svg') }}" alt="" class="w-full h-full object-contain">
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/10.webp') }}" alt="" class="w-full h-full object-contain">
     </div>
 
     {{-- Bottom-Right Corner --}}
     <div class="absolute bottom-0 right-0 w-56 h-56 opacity-60">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/11.svg') }}" alt=""
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/11.webp') }}" alt=""
             class="w-full h-full object-contain">
     </div>
 
     {{-- Bottom-Right Mid --}}
     <div class="absolute bottom-32 right-24 w-44 h-44 opacity-50">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/12.svg') }}" alt=""
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/12.webp') }}" alt=""
             class="w-full h-full object-contain">
     </div>
 
     {{-- Center Top --}}
     <div class="absolute top-16 left-1/2 transform -translate-x-1/2 w-40 h-40 opacity-30">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/13.svg') }}" alt=""
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/13.webp') }}" alt=""
             class="w-full h-full object-contain">
     </div>
 
     {{-- Center Bottom --}}
     <div class="absolute bottom-16 left-1/2 transform -translate-x-1/2 w-40 h-40 opacity-30">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/14.svg') }}" alt=""
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/14.webp') }}" alt=""
             class="w-full h-full object-contain">
     </div>
 
     {{-- Floating Elements --}}
     <div class="absolute top-1/4 left-1/4 w-24 h-24 opacity-40 animate-pulse">
-        <img src="{{ Storage::url('web/ASET VISUAL/SVG/15.svg') }}" alt=""
+        <img src="{{ Storage::url('web/ASET VISUAL/WEBP/15.webp') }}" alt=""
             class="w-full h-full object-contain">
     </div>
 </div>
