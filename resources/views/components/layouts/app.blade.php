d2f1c1fb79c3f236d4a0446932f67cd0915aac91
RizqiA17 <asyriansyah@gmail.com>
2025-10-08 23:44:40 +0700

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.app.header :title="$title ?? null">
-    <flux:main class="z-[1] pt-4 mb-28">
+    <flux:main class="z-[1] pt-4 max-sm:px-0.5 mb-28">
         {{ $slot }}
     </flux:main>
 </x-layouts.app.header>
