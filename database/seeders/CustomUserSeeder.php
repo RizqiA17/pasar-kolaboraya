505c34f61dcd80cc18d9b6b7930de7c61dc417e3
RizqiA17 <asyriansyah@gmail.com>
2025-11-04 22:44:31 +0700

    Initialitation Data Seeder

--- database/seeders/CustomUserSeeder.php
+++ database/seeders/CustomUserSeeder.php
@@ -13,17 +13,18 @@ class CustomUserSeeder extends Seeder
      */
     public function run(): void
     {
+	$email = 'admin@pasar-kolaboraya.com'
         // Check if test user already exists
-        if (User::where('email', 'test@gmail.com')->exists()) {
-            $this->command->info('Test user already exists. Skipping test user creation.');
+        if (User::where('email', $email)->exists()) {
+            $this->command->info('User already exists. Skipping user creation.');
             return;
         }
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@gmail.com',
+            'name' => 'Super Admin',
+            'email' => $email,
             'password' => bcrypt('1234567890'),
-            'role' => 'user',
+            'role' => 'super_admin',
         ]);
     }
 }
---
+++ database/seeders/MasterDataSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class MasterDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->call([
+            SystemSettingSeeder::class,
+            InterestSeeder::class,
+            SkillSeeder::class,
+            ContributionSeeder::class,
+            EventCategorySeeder::class,
+            PeranSeeder::class,
+	    CostumUserSeeder::class,
+        ]);
+    }
+}
